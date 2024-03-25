705c0b181260669355090bbcc4d67035afcbcde2
Dendosha <thenocho22@gmail.com>
2024-03-25 23:24:34 +0500

    feat: add html markup, styles and js for testimonials section

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,36 @@
 import Tabs from "./libs/a11y-tabs/a11yTabs.js"
 
+import Swiper from 'swiper'
+import { Navigation, Pagination, EffectCards } from 'swiper/modules'
+
 const pageTabs = document.querySelectorAll('[data-tabs="tabs"]')
 pageTabs.forEach(tabs => {
 	new Tabs(tabs, {
 
 	})
+})
+
+const swiper = new Swiper('.swiper', {
+	modules: [Navigation, Pagination, EffectCards],
+
+	effect: 'cards',
+	grabCursor: true,
+
+	cardsEffect: {
+		rotate: false,
+		perSlideOffset: 10,
+		slideShadows: false,
+	},
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 })
